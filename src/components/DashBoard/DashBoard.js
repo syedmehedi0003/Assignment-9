4cc2fb2e921dc0e84825d46c03874e1fc38ad931
syedmehedi0003 <mehedihasanbahar.com>
2022-06-05 14:13:27 +0600

    clone

--- src/components/DashBoard/DashBoard.js
+++ src/components/DashBoard/DashBoard.js
@@ -12,12 +12,12 @@ const DashBoard = () => {
         <div className='row mt-4'>
             <h2 className='my-4'>This is DashBoard</h2>
 
-            <div className='chart-width  col-lg-6 col-12 '>
+            <div className='col-lg-6 col-12 '>
                 <MyLineChart></MyLineChart>
             </div>
 
 
-            <div className='chart-width  col-lg-6 col-12 '>
+            <div className='col-lg-6 col-12 '>
                 <MyBarChart></MyBarChart>
             </div>
 
